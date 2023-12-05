1aa38fed8c0b20073d5795e6c69217138b807096
Andriamahaly Loiqua <hei.loiqua@gmail.com>
2023-12-05 03:19:49 +0300

    modification script sql

--- src/main/resources/Transaction.sql
+++ src/main/resources/Transaction.sql
@@ -3,20 +3,36 @@ CREATE TABLE IF NOT EXISTS transactions (transaction_id SERIAL PRIMARY KEY,
                                                                                           description TEXT, amount NUMERIC(10,2) NOT NULL,
                                                                                                                                  currency_id INT NOT NULL,
                                                                                                                                                  transaction_date DATE NOT NULL,
-                                         FOREIGN KEY (account_id) REFERENCES accounts(id));
+                                         FOREIGN KEY (account_id) REFERENCES accounts(id_account));
 
+-- Modification de la définition de la table transactions pour autoriser NULL dans account_id
+
+ALTER TABLE transactions
+ALTER COLUMN account_id
+DROP NOT NULL;
+
+-- Puis, vous pouvez réessayer votre insertion avec des valeurs NULL
 
 INSERT INTO transactions (account_id, description, amount, currency_id, transaction_date)
-VALUES (1,
+VALUES (4,
         'Purchase 1',
-        100,
+        100.50,
         1,
-        '2023-01-01'), (2,
+        '2023-01-01'), (4,
                         'Purchase 2',
-                        50,
-                        2,
-                        '2023-01-02'), (3,
-                                        'Purchase 3',
-                                        200,
-                                        1,
-                                        '2023-01-03');
+                        75.25,
+                        1,
+                        '2023-01-02'), (5,
+                                        'Expense 1',
+                                        50.00,
+                                        2,
+                                        '2023-01-03'), (5,
+                                                        'Purchase 3',
+                                                        120.75,
+                                                        2,
+                                                        '2023-01-04'), (NULL,
+                                                                        'Withdrawal 1',
+                                                                        30.00,
+                                                                        1,
+                                                                        '2023-01-05');
+
